c60c6c5cc8edaa5b5b7246ba475f6aac428fc68b
ki_blank <lbin7310@gmail.com>
2019-09-25 14:43:11 +0900

    Add middleware, pageTitle and Apply res.locals'

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,7 +3,7 @@ import routes from '../routes';
 import { users, userDetail, editProfile, changePassword } from '../controller/userController';
 
 const userRouter = express.Router();
-// userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.home, users);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,10 +3,10 @@ import routes from '../routes';
 import { videos, upload, videoDetail, editVideo, deleteVideo } from '../controller/videoController';
 
 const videoRouter = express.Router();
-videoRouter.get(routes.home, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.home, videos);
 
 export default videoRouter;
