2af89abb984e079f83f23fcbb695f8d52c8428c1
ki_blank <lbin7310@gmail.com>
2019-09-27 18:54:37 +0900

    3.4

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
 import "./db";
 import app from './app';
+import dotenv from "dotenv";
+dotenv.config()
+import "./models/Video";
+import "./models/Comment";
 
-
-const PROT = 4000;
-
+const PROT = process.env.PORT || 4000;
 
 const handleListening = () => 
   console.log(`✅ Listening on: http://localhost:${PROT}`);
