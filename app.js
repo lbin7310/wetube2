012e064bfa97af1330bd35178ac994223c56e034
ki_blank <lbin7310@gmail.com>
2019-10-03 11:15:59 +0900

    4.3

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
 app.use(helmet());
-app.set("view engine", "pug")
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
